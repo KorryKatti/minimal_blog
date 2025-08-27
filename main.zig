8d97a5dd479ccdade18a016a352141acbe233285
korrykatti <143781663+KorryKatti@users.noreply.github.com>
2025-08-27 09:47:57 +0530

    followed a guide to make a basic web server

---
+++ main.zig
@@ -0,0 +1,30 @@
+const std = @import("std");
+const SocketConf = @import("config.zig");
+const Request = @import("request.zig");
+const stdout = std.io.getStdOut().writer();
+const Method = Request.Method;
+const Response = @import("response.zig");
+const Map = std.static_string_map.StaticStringMap;
+
+pub fn main() !void {
+    const socket = try SocketConf.Socket.init();
+    try stdout.print("Server address : {any}\n",.{socket._address});
+    var server = try socket._address.listen(.{});
+    const connection = try server.accept();
+
+    var buffer: [1000]u8 = undefined;
+    for (0..buffer.len) |i| {
+        buffer[i] = 0;
+    }
+    try  Request.read_request(connection, buffer[0..buffer.len]);
+    const request = Request.parse_request(
+        buffer[0..buffer.len]
+    );
+    if (request.method == Method.GET) {
+        if (std.mem.eql(u8, request.uri,"/")){
+            try Response.send_200(connection);
+        }else{
+            try Response.send_404(connection);
+        }
+    }
+}
