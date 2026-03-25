3444d852b1f597824bd761bd99ebebddcfe2c5e9
korrykatti <143781663+KorryKatti@users.noreply.github.com>
2026-03-25 15:29:23 +0530

    y

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"io/ioutil"
 //	"os"
 	"context"
 	"net"
@@ -22,6 +23,23 @@ func main() {
 	mux.HandleFunc("/hello", getHello)
 
 	ctx, cancelCtx := context.WithCancel(context.Background())
+
+	server := &http.Server{
+		Addr:":3333",
+		Handler:mux,
+		BaseContext: func(l net.Listener)context.Context {
+			ctx = context.WithValue(ctx,keyServerAddr,l.Addr().String())
+			return ctx
+		},
+	}
+
+	err := server.ListenAndServe()
+	if errors.Is(err, http.ErrServerClosed) {
+		fmt.Printf("server closed\n")
+	} else if err != nil {
+		fmt.Printf("error listening for server: %s\n", err)
+	}
+	
 
 	serverOne := &http.Server{
 		Addr:    ":3456",
@@ -67,6 +85,18 @@ func getRoot(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
 
 	fmt.Printf("%s: got / request\n",ctx.Value(keyServerAddr))
+	hasFirst := r.URL.Query().Has("first")
+	first := r.URL.Query().Get("first")
+	hasSecond :=  r.URL.Query().Has("second")
+	second := r.URL.Query().Get("second")
+
+	body,err := ioutil.ReadAll(r.Body)
+	if err != nil{
+		fmt.Printf("couldn't read body %s\n",err)
+	}
+
+	fmt.Println("%s: got /request first(%t)=%s, second(%t)=%s, body:\n%s\n",ctx.Value(keyServerAddr),hasFirst,first,hasSecond,second,string(body))
+
 	io.WriteString(w, "this is my website\n")
 }
 
@@ -74,6 +104,13 @@ func getHello(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
 
 	fmt.Printf("%s got /hello request\n",ctx.Value(keyServerAddr))
-	io.WriteString(w, " Hello, HTTP!\n")
-}
 
+	myName := r.PostFormValue("myName")
+	if myName == ""{
+		w.Header().Set("x-missing-field","myName")
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	io.WriteString(w,fmt.Sprintf("Hello, %s\n", myName))
+}
